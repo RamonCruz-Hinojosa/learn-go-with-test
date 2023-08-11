c034e12cc9f4cb711db89b5b51a5653ae076fac1
Tomas <tleffew1994@gmail.com>
2023-08-11 10:39:40 -0500

    iteration section done need to write exampletest

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,5 +1,10 @@
 package iteration
 
-func Repeat(character string) string {
-	return ""
+func Repeat(character string, times int) string {
+	var repeated string
+	for i := 0; i < times; i++ {
+		repeated += character
+		//using := is declaring and assigning in one go while = is just assigning := would not work on this line
+	}
+	return repeated
 }
