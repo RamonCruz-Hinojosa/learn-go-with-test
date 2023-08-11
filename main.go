0b9e12d9d1d18f4318d304fdd7580757118cddfd
Ramon <bairdwins@gmail.com>
2023-08-11 16:02:42 -0500

    exercises on basic go methods in main

--- main.go
+++ main.go
@@ -1,6 +1,46 @@
 package main
 
+import "fmt"
+
+// practice exercises here
 func main() {
+
+	// //strings
+	// // var nameOne string = "mario"
+	// // var nameTwo = "luigi"
+	// // var nameThree string
+	// // fmt.Println(nameOne, nameTwo, nameThree)
+
+	// // nameOne = "peach"
+	// // nameThree = "bowser"
+	// // fmt.Println(nameOne, nameTwo, nameThree)
+
+	// // nameFour := "yoshi"
+	// // fmt.Println(nameFour)
+
+	// //ints
+	// var ageOne int = 20
+	// var ageTwo = 30
+	// ageThree := 40
+
+	// fmt.Println(ageOne, ageTwo, ageThree)
+
+	// // //bits and memory
+	// // var numOne int8 = 25
+	// // var numTwo int8 = -128
+	// // var numThree uint = 24
+
+	// var scoreOne float32 = -1.5
+	// var scroeTwo float64 = 1234123412341234.7
+	// scoreThree := 1.5
+
+	//Print
+	fmt.Print("hello, ")
+	fmt.Print("world! \n")
+	fmt.Print("world! \n")
+
+	fmt.Println("hello ninjas!")
+
 }
 
 func Hello() {}
