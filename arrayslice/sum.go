49c770165f4d3a1ed304c80cd3943d572d52d633
Ramon <bairdwins@gmail.com>
2023-08-14 14:13:57 -0500

    changed sum func to accept slices instead of arrays refactor tests

--- arrayslice/sum.go
+++ arrayslice/sum.go
@@ -1,9 +1,9 @@
 package arrayslice
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
