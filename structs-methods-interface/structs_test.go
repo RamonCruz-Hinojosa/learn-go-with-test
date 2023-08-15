820f99349aa7bd871e3385ad95f54179ade41e87
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2023-08-15 13:59:15 -0600

    using struct to help identify what functions in structs go are used for

--- structs-methods-interface/structs_test.go
+++ structs-methods-interface/structs_test.go
@@ -3,10 +3,26 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
+}
+
+func TestArea(t *testing.T) {
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
 }
