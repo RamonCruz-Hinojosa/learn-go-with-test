d3b319f6f026b54dc034c151043c64a0db3d89bc
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2023-08-16 13:30:11 -0600

    removed all exercise code from main.go

--- pointersErrors/wallet.go
+++ pointersErrors/wallet.go
@@ -1,6 +1,9 @@
 package pointersErrors
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 type Wallet struct {
@@ -11,8 +14,14 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
 	w.balance -= amount
+	return nil
 }
 
 //pointers to structs (*Wallet is the pointer here and without it Wallet is just a copy and not changing the real values) are automatically dereferenced
