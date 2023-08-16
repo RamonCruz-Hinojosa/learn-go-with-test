e85fe0729f0e522a3f7a724f62b178cb3409a4ad
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2023-08-16 10:13:33 -0600

    moved structs/methods to correct file base func and tests for pointers

--- pointersErrors/wallet_test.go
+++ pointersErrors/wallet_test.go
@@ -1,1 +1,16 @@
 package pointersErrors
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
