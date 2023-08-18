45f8f85debf861ac524f438040a8e3dbd6525c41
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2023-08-18 16:45:53 -0600

    greet func used to send a msg to local host through greet handler and main funcs

--- dependency/di.go
+++ dependency/di.go
@@ -1,7 +1,20 @@
-package dependency
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
-func Greet(name string) {
-	fmt.Printf("Hello, %s", name)
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
