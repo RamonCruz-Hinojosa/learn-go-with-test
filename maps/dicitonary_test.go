4f09867577dc5ad204903e508fb90a26e3123d9a
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2023-08-18 14:29:13 -0600

    added delete func and tests all passing maps finished

--- maps/dicitonary_test.go
+++ maps/dicitonary_test.go
@@ -42,14 +42,39 @@ func TestAdd(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
+
+func TestDelete(t *testing.T) {
 	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
+	dictionary := Dictionary{word: "test definition"}
 
-	dictionary.Update(word, newDefinition)
+	dictionary.Delete(word)
 
-	assertDefinition(t, dictionary, word, newDefinition)
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
