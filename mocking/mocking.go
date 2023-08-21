29c7743e4d592caee292bf943b46f17b356856a9
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2023-08-21 16:49:22 -0600

    adding sleeper interface and spysleeper struct

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,7 +4,20 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
 
 const finalWord = "Go!"
 const countdownStart = 3
@@ -12,6 +25,7 @@ const countdownStart = 3
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Fprint(out, finalWord)
 }
